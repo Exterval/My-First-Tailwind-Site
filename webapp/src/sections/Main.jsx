488c9d5d31ecfda8efa0943b2495c48abfe05541
Exterval <treblmao@gmail.com>
2025-11-06 18:21:59 +0800

    tweaked styles 1

--- webapp/src/sections/Main.jsx
+++ webapp/src/sections/Main.jsx
@@ -5,9 +5,11 @@ export default function Main() {
                 <h1 className="text-center text-4xl">We make meaningful applications.</h1>
                 <p className="text-2xl text-zinc-500 text-center mb-5">Lorem ipsum dolor sit.</p>
                 <div className="mx-auto flex flex-row">
-                    <button className="bg-black text-white p-4 rounded-2xl hover:bg-white hover:text-black hover:-translate-y-1 hover:shadow-2xs" id="" style={{transition: '0.3s ease-in'}}>Contact Us!</button>
+                    <button className="bg-black text-white p-4 rounded-2xl hover:bg-white hover:text-black hover:-translate-y-1 hover:shadow-2xs cursor-pointer
+                    transition-all duration-300 ease-out border-white" id="">Contact Us!</button>
                     <div style={{width: '1px', height:'100%'}} className="bg-zinc-500 mx-3"></div>
-                    <button className="bg-white text-black p-4 rounded-2xl hover:bg-black hover:text-white hover:-translate-y-1 hover:shadow-2xs" id="" style={{transition: '0.3s ease-in'}}>Sample text</button>
+                    <button className="bg-white text-black p-4 rounded-2xl hover:bg-black hover:text-white hover:-translate-y-1 hover:shadow-2xs cursor-pointer
+                    transition-all duration-300 ease-out border-white" id="">Sample text</button>
                 </div>
             </div> 
         </section>
