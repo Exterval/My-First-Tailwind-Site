555574bc94da424461face02f62c07663b26d098
Exterval <treblmao@gmail.com>
2025-11-09 17:34:07 +0800

    tweaked hero section, added placeholder image

--- webapp/src/sections/Main.jsx
+++ webapp/src/sections/Main.jsx
@@ -1,16 +1,26 @@
 export default function Main() {
     return(
         <section id="main">
-            <div className="bg-linear-to-r from-zinc-200 to-zinc-500 w-full flex flex-col justify-center my-4" style={{height: '450px'}}>
-                <h1 className="text-center text-4xl">We make meaningful applications.</h1>
-                <p className="text-2xl text-zinc-500 text-center mb-5">Lorem ipsum dolor sit.</p>
-                <div className="mx-auto flex flex-row">
-                    <button className="bg-black text-white p-4 rounded-2xl hover:bg-white hover:text-black hover:-translate-y-1 hover:shadow-2xs cursor-pointer
-                    transition-all duration-300 ease-out border-white" id="">Contact Us!</button>
-                    <div style={{width: '1px', height:'100%'}} className="bg-zinc-500 mx-3"></div>
-                    <button className="bg-white text-black p-4 rounded-2xl hover:bg-black hover:text-white hover:-translate-y-1 hover:shadow-2xs cursor-pointer
-                    transition-all duration-300 ease-out border-white" id="">Sample text</button>
+            <div className="bg-linear-to-r from-zinc-200 to-zinc-500 w-full flex flex-row justify-around items-center my-4" style={{height: '500px'}}>
+                <div className="">
+                    <h1 className="text-4xl">We make meaningful applications.</h1>
+                    <p className="text-2xl text-zinc-500 mb-5 mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, non.</p>
+                    <div className="mx-auto flex flex-row">
+                        <button className="bg-black text-white p-4 rounded-2xl hover:bg-white hover:text-black hover:-translate-y-1 hover:shadow-2xs cursor-pointer
+                        transition-all duration-300 ease-out border-white" id="">Contact Us!</button>
+                    </div>
                 </div>
+                <div className="w-fit">
+                    <img src="src/assets/placeholder.jpg" alt="Image" className="object-contain w-full" />
+                </div>
+                {/* <div className="">
+                    <h1 className="text-center text-4xl">We make meaningful applications.</h1>
+                    <p className="text-2xl text-zinc-500 text-center mb-5 mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, non.</p>
+                    <div className="mx-auto flex flex-row">
+                        <button className="bg-black text-white p-4 rounded-2xl hover:bg-white hover:text-black hover:-translate-y-1 hover:shadow-2xs cursor-pointer
+                        transition-all duration-300 ease-out border-white" id="">Contact Us!</button>
+                    </div>
+                </div>         */}
             </div> 
         </section>
     )
