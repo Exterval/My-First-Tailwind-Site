bb0a3578b6a51e8763a1fe0468392a16944b83e2
Exterval <treblmao@gmail.com>
2025-11-10 21:31:41 +0800

    tweaked styles 6

--- webapp/src/components/NavBar.jsx
+++ webapp/src/components/NavBar.jsx
@@ -4,9 +4,9 @@ export default function NavBar() {
             <img src="src/assets/logo.png" className="object-contain w-full" style={{textAlign: 'center'}} alt="Logo Image" />
             <ul className="flex flex-row items-center justify-evenly w-full rounded-bl-3xl backdrop-blur-lg border border-white/5
             transition-all duration-300 ease-in-out">
-                <li className="hover:bg-zinc-500/20 rounded-4xl pl-5 py-2 cursor-pointer" style={{transition: '0.2s ease-in'}}><a href="#main" className="hover:text-zinc-200 text-sm lg:text-base md:text-base" style={{transition: '0.2s ease-in-out'}}>Home</a></li>
-                <li className="hover:bg-zinc-500/20 rounded-4xl pl-5 py-2 cursor-pointer" style={{transition: '0.2s ease-in'}}><a href="#section" className="hover:text-zinc-200 text-sm lg:text-base md:text-base" style={{transition: '0.2s ease-in-out'}}>About</a></li>
-                <li className="hover:bg-zinc-500/20 rounded-4xl pl-5 py-2 cursor-pointer" style={{transition: '0.2s ease-in'}}><a href="#service" className="hover:text-zinc-200 text-sm lg:text-base md:text-base" style={{transition: '0.2s ease-in-out'}}>Services</a></li>
+                <li className="rounded-4xl py-2" style={{transition: '0.2s ease-in'}}><a href="#main" className="hover:text-zinc-200 text-sm lg:text-base md:text-base" style={{transition: '0.2s ease-in-out'}}>Home</a></li>
+                <li className="rounded-4xl py-2" style={{transition: '0.2s ease-in'}}><a href="#section" className="hover:text-zinc-200 text-sm lg:text-base md:text-base" style={{transition: '0.2s ease-in-out'}}>About</a></li>
+                <li className="rounded-4xl py-2" style={{transition: '0.2s ease-in'}}><a href="#service" className="hover:text-zinc-200 text-sm lg:text-base md:text-base" style={{transition: '0.2s ease-in-out'}}>Services</a></li>
             </ul>
         </div>
     )
